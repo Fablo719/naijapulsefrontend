6b59bfb34e4da696270c15b093cdd958020bc06a
fablo <fabinuoluwadarasimi8@gmail.com>
2026-06-01 18:35:15 -0400

    updated, top notch uiux

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -22,7 +22,7 @@ function LandingPage() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/login">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/login">Write</Link>
